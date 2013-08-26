7abe88ee054c82fc9cb4cd7b96e926aff47a4de1
sigurlu <sigurd.l@gmail.com>
2013-08-26 11:10:47 +0200

    Added main class

---
+++ src/Project1/Quarto.java
@@ -0,0 +1,15 @@
+package Project1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sigurd
+ * Date: 8/26/13
+ * Time: 10:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Quarto {
+
+    public static void main(String[] args) {
+        System.out.println("hei");
+    }
+}
