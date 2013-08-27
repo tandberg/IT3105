10e7919f040a6e14ec0d499d93f2b79f6960454d
Øystein Tandberg <tandeey@gmail.com>
2013-08-27 12:06:49 +0200

    random player

---
+++ src/Project1/RandomPlayer.java
@@ -0,0 +1,15 @@
+package Project1;
+
+public class RandomPlayer implements Player {
+
+	private Quarto game;
+	
+	public RandomPlayer(Quarto game) {
+		this.game = game;
+	}
+
+	@Override
+	public void move() {
+		
+	}
+}
