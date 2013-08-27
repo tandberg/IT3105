cadad08c5925c79944b9118c5e8243fd26b42741
sigurlu <sigurd.l@gmail.com>
2013-08-27 13:31:54 +0200

    Class for human player

---
+++ src/Project1/HumanPlayer.java
@@ -0,0 +1,35 @@
+package Project1;
+
+import java.util.Scanner;
+
+public class HumanPlayer extends Player {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public HumanPlayer(Quarto game) {
+        super(game);
+    }
+
+    @Override
+    public void placeBrick(int index) {
+        Brick b = game.getBricks().get(index);
+
+        System.out.println("Select row:");
+        int row = scanner.nextInt();
+
+        System.out.println("Select column:");
+        int column = scanner.nextInt();
+
+        if (!game.setPiece(row, column, index))                 {
+            System.out.println("You can't place a brick there..");
+            placeBrick(index);
+        }
+    }
+
+
+    @Override
+    public int pickOpponentsBrick() {
+        System.out.println("Select brick for opponent:");
+        return scanner.nextInt();
+    }
+}
