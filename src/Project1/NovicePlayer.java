7dea1be68b8ec584aded2b64183fadccd3940400
Øystein Tandberg <tandeey@gmail.com>
2013-08-29 14:21:25 +0200

    Novice player

---
+++ src/Project1/NovicePlayer.java
@@ -0,0 +1,21 @@
+package Project1;
+
+public class NovicePlayer extends Player {
+	
+	public NovicePlayer(Quarto game) {
+		super(game);
+	}
+
+	@Override
+	public void placeBrick(int brickIndex) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int pickOpponentsBrick() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
