40d294a4ed3e7c4db05a00130f334e039f2a06e3
Øystein Tandberg <tandeey@gmail.com>
2013-09-05 15:31:01 +0200

    NovicePlayer improvement. Wins 9/10 against random

--- src/Project1/NovicePlayer.java
+++ src/Project1/NovicePlayer.java
@@ -1,6 +1,10 @@
 package Project1;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Random;
+
+//import Project1.MiniMaxPlayer.Move;
 
 public class NovicePlayer extends Player {
 	
@@ -15,18 +19,16 @@ public class NovicePlayer extends Player {
 	public void placeBrick(int brickIndex) {
 		Quarto tempGame = game.copy();
 		
-		for (int i = 0; i < Quarto.BOARD_SIZE; i++) {
-			for (int j = 0; j < Quarto.BOARD_SIZE; j++) {
-				if(tempGame.setPiece(i, j, brickIndex)) {
-					if(tempGame.isComplete() == Quarto.WINNER) {
-						game.setPiece(i, j, brickIndex);
-						return;
-					}
-				}
-				tempGame.removePiece(i, j, brickIndex);
+		for (Move move : this.getPossibleMoves(tempGame)) { // Loops through all possible moves to check if there is a immediate winner move.
+			tempGame.setPiece(move.row, move.column, brickIndex);
+			if(tempGame.isComplete() == Quarto.WINNER) {
+				game.setPiece(move.row, move.column, brickIndex);
+				return;
 			}
+			tempGame.removePiece(move.row, move.column, brickIndex);
 		}
-
+		
+		// Else just place it randomly
 		while(!game.setPiece(random.nextInt(Quarto.BOARD_SIZE), random.nextInt(Quarto.BOARD_SIZE), brickIndex));
 		
 	}
@@ -41,8 +43,6 @@ public class NovicePlayer extends Player {
 					if(tempGame.setPiece(i, j, brickIndex)) {
 						if(tempGame.isComplete() != Quarto.NOT_FINISHED) {
 							brickIsOk = false;
-							//game.setPiece(i, j, brickIndex);
-//							return brickIndex;
 						}
 					}
 					tempGame.removePiece(i, j, brickIndex);
@@ -53,8 +53,31 @@ public class NovicePlayer extends Player {
 			}
 			brickIsOk = true;
 		}
+
 		return 0;
 	}
+	
+	private Collection<Move> getPossibleMoves(Quarto game) {
+        Brick[][] board = game.getBoard();
+        Collection<Move> possibleMoves = new HashSet<Move>();
+        for (int i = 0; i < Quarto.BOARD_SIZE; i++) {
+            for (int j = 0; j < Quarto.BOARD_SIZE; j++) {
+                if (board[i][j] == null) {
+                    possibleMoves.add(new Move(i, j));
+                }
+            }
+        }
+        return possibleMoves;
+    }
+	
+	private class Move {
+        int row, column;
+
+        public Move(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+    }
 	
 	public String stringify() {
 		return "NovicePlayer";
