aefa7c7915b9d43157462938f5d92c4ff4a3374b
Øystein Tandberg <tandeey@gmail.com>
2013-10-03 13:27:22 +0200

    LSM abstract class

--- src/project2/LocalStateManager.java
+++ src/project2/LocalStateManager.java
@@ -1,5 +1,24 @@
 package project2;
 
+import java.util.List;
+
 public abstract class LocalStateManager {
 
+	// 1.
+	public abstract void initialize();
+	
+	// 2.
+	public abstract void mofifyState();
+	public abstract void modifyRandomState();
+	public abstract void modifyIntelligentState();
+	
+	// 3.
+	public abstract List<State> generateSuccessorState();
+	
+	// 4.
+	public abstract double evaluate(State state);
+	
+	// 5.
+	public abstract void displayState(State state);
+	
 }
