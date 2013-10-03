4ba41c722d1e8b28a8ad602ed0bd9d8405959f7f
Øystein Tandberg <tandeey@gmail.com>
2013-10-03 14:27:34 +0200

    boilerplate files

---
+++ src/project2/GraphColorStateManager.java
@@ -0,0 +1,52 @@
+package project2;
+
+import java.util.List;
+
+public class GraphColorStateManager extends LocalStateManager {
+
+	public GraphColorStateManager() {
+		
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mofifyState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifyRandomState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifyIntelligentState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<State> generateSuccessorState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double evaluate(State state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void displayState(State state) {
+		// TODO Auto-generated method stub
+		
+	}
+}
