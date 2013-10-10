fc4fc3eba40086ea27cfe5f549768282077b158f
sigurlu <sigurd.l@gmail.com>
2013-10-10 13:34:32 +0200

    dasda

--- src/project2/GeneralPuzzleSolver.java
+++ src/project2/GeneralPuzzleSolver.java
@@ -2,4 +2,8 @@ package project2;
 
 public class GeneralPuzzleSolver {
 
+   public static void main(String[] args) {
+       System.out.println("pelle");
+   }
+
 }
---
+++ src/project2/KQueensStateManager.java
@@ -0,0 +1,81 @@
+package project2;
+
+import java.util.List;
+
+public class KQueensStateManager extends LocalStateManager {
+	
+	private int ksize;
+	private boolean[][] board;
+	private int queensOnBoard;
+	
+	
+	
+
+
+	public void initialize(int size) {
+		ksize = size;
+		board = new boolean[ksize][ksize];
+		queensOnBoard = 0;
+		
+	}
+
+    @Override
+    public void initialize() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+	public void mofifyState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifyRandomState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifyIntelligentState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<State> generateSuccessorState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double evaluate(State state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private boolean checkColumns() {
+		for (int i = 0; i < ksize; i++) {
+			for (int j = 0; j < ksize; j++) {
+				
+			}
+			
+		}
+        return false;
+	}
+	
+	private boolean checkRows() {
+		for (int i = 0; i < ksize; i++) {
+			
+		}
+        return false;
+	}
+	
+
+	@Override
+	public void displayState(State state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
