6644807c488842caa23bcacc447a52e6694e782b
Øystein Tandberg <tandeey@gmail.com>
2013-10-10 14:47:26 +0200

    login in GC state

--- src/project2/GraphColorState.java
+++ src/project2/GraphColorState.java
@@ -1,8 +1,72 @@
 package project2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphColorState extends State {
 
-	public GraphColorState() {
-		// nabomatriseq
+    private boolean[][] matrix;
+    private int numberOfNodes;
+
+	public GraphColorState(boolean[][] matrix, int colors) {
+		this.matrix = matrix;
+        this.numberOfNodes = matrix.length;
 	}
+
+    private static int convertNodeToInt(char node) {
+        return Character.toLowerCase(node) - 'a';
+    }
+
+    private static char convertIntToNode(int node) {
+        return (char) (node + (int)'a');
+    }
+
+    public boolean isNeighbour(char node1, char node2) {
+        int nodeA = convertNodeToInt(node1);
+        int nodeB = convertNodeToInt(node2);
+
+        if(nodeA > this.numberOfNodes || nodeB > this.numberOfNodes)
+            throw new IllegalArgumentException();
+
+        return this.matrix[nodeA][nodeB] || this.matrix[nodeB][nodeA];
+    }
+
+    public List<Character> getNeighbours(char nodeName) {
+        int node = convertNodeToInt(nodeName);
+        List<Character> neighbours = new ArrayList<Character>();
+
+        for (int i = 0; i < this.numberOfNodes; i++) {
+            if(this.matrix[node][i] && node != i) {
+                neighbours.add(convertIntToNode(i));
+            }
+        }
+        return neighbours;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < this.numberOfNodes; i++) {
+            sb.append(convertIntToNode(i) + ": " + getNeighbours(convertIntToNode(i)) + "\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        boolean[][] matrix = {
+                {true, true, false, true, false},
+                {true, true, true, false, true},
+                {false, true, true, false, false},
+                {true, false, false, true, false},
+                {false, true, false, false, true}
+        };
+
+        GraphColorState state = new GraphColorState(matrix, 3);
+        System.out.println(state.isNeighbour('d', 'c'));
+        System.out.println(state);
+    }
+
+
 }
