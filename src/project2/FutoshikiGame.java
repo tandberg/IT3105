421beaf4a0378cf34add2093b5ce4ea27c9f0616
Øystein Tandberg <tandeey@gmail.com>
2013-10-23 17:40:31 +0200

    games for F

--- src/project2/FutoshikiGame.java
+++ src/project2/FutoshikiGame.java
@@ -5,7 +5,31 @@ import java.util.Set;
 
 public class FutoshikiGame {
 
-    public static void initializeEasyBoard(Map<String, String> lessThan, Map<String, String> greaterThan, Set<String> constraints) {
+    public Set<String> constraints;
+    public int[][] board;
+
+    public FutoshikiGame(int difficulty) {
+        switch (difficulty) {
+            case 1:
+                initializeEasyBoard();
+                board = getEasyBoard();
+                break;
+            case 2:
+                initializeMediumBoard();
+                board = getMediumBoard();
+                break;
+            case 3:
+                initializeHardBoard();
+                board = getHardBoard();
+                break;
+            default:
+
+        }
+    }
+
+
+
+    public void initializeEasyBoard() {
 
         constraints.add("0.0>1.0");
         constraints.add("1.1>1.2");
@@ -23,9 +47,55 @@ public class FutoshikiGame {
         return b;
     }
 
-    private String getConstraintString(int rowA, int colA, int rowB, int colB, boolean greater) {
-        return rowA + "." + colA + ">" + rowB + "." + colB;
+    public void initializeMediumBoard() {
+
+        constraints.add("0.1>0.2");
+        constraints.add("0.3>0.4");
+
+        constraints.add("1.0>1.1");
+        constraints.add("1.2>0.2");
+        constraints.add("1.3>0.3");
+        constraints.add("0.4>1.4");
+        constraints.add("1.5>0.5");
+
+        constraints.add("1.0>2.0");
+        constraints.add("2.1>2.0");
+        constraints.add("2.1>1.1");
+        constraints.add("2.2>2.3");
+        constraints.add("1.3>2.3");
+        constraints.add("2.5>2.4");
+        constraints.add("2.5>1.5");
+
+        constraints.add("3.1>3.0");
+        constraints.add("2.2>3.2");
+        constraints.add("3.3>2.3");
+
+        constraints.add("5.0>4.0");
+        constraints.add("5.3>5.2");
+
     }
 
+    public static int[][] getMediumBoard() {
+        int[][] b = {
+                {5, 0, 0, 0, 0, 0},
+                {0, 2, 0, 4, 0, 3},
+                {0, 0, 2, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 5, 0, 0},
+                {0, 0, 0, 0, 0, 0}
+        };
+        return b;
+    }
 
+    private void initializeHardBoard() {
+    }
+
+    public static int[][] getHardBoard() {
+        int[][] b = {
+                {0, 0, 0, 0, 0, 0},
+                {}
+        };
+
+        return b;
+    }
 }
