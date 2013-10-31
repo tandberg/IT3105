bb23e6c1eef2366f70a5d931a367037e594941d8
Øystein Tandberg <tandeey@gmail.com>
2013-10-31 14:07:29 +0100

    update functions in particle class

--- src/project3/Particle.java
+++ src/project3/Particle.java
@@ -9,80 +9,72 @@ public class Particle {
     private final static double c1 = 1;
     private final static double c2 = 1;
     private Random random;
-/*
-    private double posX;
-    private double velocity;
-    private double local_best_position;
-    private double prevVelocity;
-    private double prevPosition;
-    */
 
     private int dimensions;
     private double[] positions;
-    private double[] velocitys;
+    private double[] velocities;
 
     private double[] bestLocalPositions;
 
     public Particle(int n) {
         this.dimensions = n;
         positions = new double[n];
-        velocitys = new double[n];
+        velocities = new double[n];
         bestLocalPositions = new double[n];
 
+        fillRandomPositions();
+        fillRandomVelocities();
+        updateBestLocalPositions();
+
         random = new Random();
+    }
+
+    private void updateBestLocalPositions() {
+        for (int i = 0; i < bestLocalPositions.length; i++) {
+            bestLocalPositions[i] = Math.min(bestLocalPositions[i], positions[i]);
+        }
+    }
+
+    private void fillRandomVelocities() {
+        for (int i = 0; i < velocities.length; i++) {
+            velocities[i] = 0; // Start speed at zero in any dimension
+        }
+    }
+
+    private void fillRandomPositions() {
+        for (int i = 0; i < positions.length; i++) {
+            positions[i] = random.nextDouble();
+        }
     }
 
     public void update() {
         updatePosition();
         updateVelocity();
+
+        updateBestLocalPositions();
+        updateGlobals();
+    }
+
+    private void updateGlobals() {
+        for (int i = 0; i < positions.length; i++) {
+            globalBestPositions[i] = Math.min(globalBestPositions[i], positions[i]);
+        }
     }
 
     private void updateVelocity() {
-        for (int i = 0; i < velocitys.length; i++) {
-            double inertia = velocitys[i];
-            double memory = c1 * random.nextDouble() * (bestLocalPositions[i] - velocitys[i]);
-            double influence = c2 * random.nextDouble() * (globalBestPositions[i] - velocitys[i]);
+        for (int i = 0; i < velocities.length; i++) {
+            double inertia = velocities[i];
+            double memory = c1 * random.nextDouble() * (bestLocalPositions[i] - velocities[i]);
+            double influence = c2 * random.nextDouble() * (globalBestPositions[i] - velocities[i]);
 
-            velocitys[i] = inertia + memory + influence;
+            velocities[i] = inertia + memory + influence;
         }
     }
 
     private void updatePosition() {
         for (int i = 0; i < positions.length; i++) {
-            positions[i] = positions[i] + velocitys[i];
+            positions[i] = positions[i] + velocities[i];
         }
     }
 
-    /*
-    public Particle(double posX, double velocity) {
-        this.random = new Random();
-        this.posX = posX;
-        this.velocity = velocity;
-
-        this.local_best_position = 0;
-
-        this.prevVelocity = this.random.nextDouble();
-        this.prevPosition = this.random.nextDouble();
-    }
-
-    public void updateVelocity() {
-        double inertia = this.velocity;
-        double memory = c1 * this.random.nextDouble() * (this.local_best_position - this.velocity);
-        double influence = c2 * this.random.nextDouble() * (global_best_position - this.velocity);
-
-        this.velocity = inertia + memory + influence;
-    }
-
-    public void updatePosition() {
-        this.posX = this.prevPosition + this.velocity;
-    }
-
-    public void update() {
-        updatePosition();
-        updateVelocity();
-    }
-
-    public String toString() {
-        return "pos: " + this.posX + " velocity: " + this.velocity;
-    }*/
 }
