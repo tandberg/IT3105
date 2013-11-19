7c978cbfb38aa7f28096d59539bd556d74a78dd8
Øystein Tandberg <tandeey@gmail.com>
2013-11-19 09:45:09 +0100

    knn class

---
+++ src/project3/task2/KNearestNeighbour.java
@@ -0,0 +1,14 @@
+package project3.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KNearestNeighbour {
+
+    public static List<Particle> algorithm(List<Particle> all) {
+        List<Particle> neighbours = new ArrayList<Particle>();
+
+
+        return neighbours;
+    }
+}
